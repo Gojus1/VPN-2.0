e9d857ceab8f639aecb7b7b8c737aecea2c9afc0
greycloudss <greycloudss>
2025-07-17 20:57:20 +0300

    test commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+struct Flags{
+	//-p
+	//-ip
+	//-pass
+	
+	Flags()
+}	
+
+
+
+void parseFlags(int argc, const char** argv, Flags userFlags) {
+	
+
+}
+
+
+int main(int argc, char* argv[]) {
+	Flags userFlags;
+	parseFlags(argc, argv);
+
+	
+
+	// graceful exit
+	
+	return 0;
+}
