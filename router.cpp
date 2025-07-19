1496a7577c6f83cdd6293cdfc5cb7ed9b0702e33
Gojus1 <mister.mec22@gmail.com>
2025-07-19 15:37:52 +0000

    semi hard

--- router.cpp
+++ router.cpp
@@ -6,67 +6,88 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <thread>
+#include <sstream>
 
 #define PORT 5000
 #define BUFSIZE 8192
 
-void pipe_data(int from, int to) {
-    char buffer[BUFSIZE];
-    int len;
-    while ((len = read(from, buffer, BUFSIZE)) > 0) {
-        send(to, buffer, len, 0);
-    }
-    shutdown(to, SHUT_WR);
-}
-
-void handle_client(int client_fd) {
-    char line[BUFSIZE];
-    int idx = 0;
-    char ch;
-    
-    // Read the host:port line
-    while (read(client_fd, &ch, 1) == 1 && ch != '\n' && idx < BUFSIZE - 1) {
-        line[idx++] = ch;
-    }
-    line[idx] = '\0';
-
-    std::string dest(line);
-    size_t colon = dest.find(':');
-    if (colon == std::string::npos) {
-        std::cerr << "[ERROR] Invalid destination format\n";
-        close(client_fd);
-        return;
-    }
-
-    std::string hostname = dest.substr(0, colon);
-    int port = std::stoi(dest.substr(colon + 1));
-
-    // Resolve host
-    hostent* server = gethostbyname(hostname.c_str());
+std::string resolve_host(const std::string& host) {
+    hostent* server = gethostbyname(host.c_str());
     if (!server) {
-        std::cerr << "[ERROR] Could not resolve host\n";
+        std::cerr << "[ERROR] Could not resolve " << host << "\n";
+        return "";
+    }
+    return inet_ntoa(*(struct in_addr*)server->h_addr);
+}
+void handle_client(int client_fd) {
+    char buffer[BUFSIZE];
+    std::string line;
+    int len = recv(client_fd, buffer, BUFSIZE - 1, 0);
+    if (len <= 0) return;
+    buffer[len] = '\0';
+
+    std::istringstream iss(buffer);
+    std::getline(iss, line);
+
+    std::string host;
+    int port = 80;
+
+    size_t sep = line.find(':');
+    if (sep != std::string::npos) {
+        host = line.substr(0, sep);
+        try {
+            port = std::stoi(line.substr(sep + 1));
+        } catch (...) {
+            std::cerr << "[ERROR] Invalid port format. Using default 80.\n";
+        }
+    } else {
+        std::cerr << "[ERROR] Invalid format. Using default values.\n";
+        host = "httpbin.org";
+    }
+
+    std::string dest_ip = resolve_host(host);
+    if (dest_ip.empty()) {
         close(client_fd);
         return;
     }
 
-    sockaddr_in dest_addr{};
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(port);
-    memcpy(&dest_addr.sin_addr, server->h_addr, server->h_length);
+    sockaddr_in dest{};
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(port);
+    inet_pton(AF_INET, dest_ip.c_str(), &dest.sin_addr);
 
     int forward_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(forward_fd, (sockaddr*)&dest_addr, sizeof(dest_addr)) < 0) {
-        std::cerr << "[ERROR] Could not connect to destination\n";
+    if (connect(forward_fd, (sockaddr*)&dest, sizeof(dest)) < 0) {
+        std::cerr << "[ERROR] Could not connect to " << dest_ip << "\n";
         close(client_fd);
         return;
     }
 
-    // Start piping data
-    std::thread t1(pipe_data, client_fd, forward_fd);
-    std::thread t2(pipe_data, forward_fd, client_fd);
-    t1.join();
-    t2.join();
+std::string full_request;
+
+std::string leftover;
+std::getline(iss, line);
+while (std::getline(iss, line)) {
+    leftover += line + "\n";
+}
+full_request += leftover;
+
+while (full_request.find("\r\n\r\n") == std::string::npos) {
+    len = recv(client_fd, buffer, BUFSIZE - 1, 0);
+    if (len <= 0) {
+        std::cerr << "[ERROR] Client closed before sending request.\n";
+        close(client_fd);
+        return;
+    }
+    buffer[len] = '\0';
+    full_request += buffer;
+}
+
+    send(forward_fd, full_request.c_str(), full_request.size(), 0);
+
+    while ((len = recv(forward_fd, buffer, BUFSIZE, 0)) > 0) {
+        send(client_fd, buffer, len, 0);
+    }
 
     close(forward_fd);
     close(client_fd);
@@ -74,25 +95,40 @@ void handle_client(int client_fd) {
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "[ERROR] Failed to create socket.\n";
+        return 1;
+    }
+
     sockaddr_in address{};
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    bind(server_fd, (sockaddr*)&address, sizeof(address));
-    listen(server_fd, 10);
+    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "[ERROR] Failed to bind socket.\n";
+        return 1;
+    }
 
-    std::cout << "[VPN PROXY] Listening on port " << PORT << "\n";
+    listen(server_fd, 10);
+    std::cout << "[VPN PROXY] Listening on port " << PORT << "...\n";
 
     while (true) {
         sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
         int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
         if (client_fd >= 0) {
-            std::thread(handle_client, client_fd).detach();
+            if (fork() == 0) {
+                close(server_fd);
+                handle_client(client_fd);
+                exit(0);
+            } else {
+                close(client_fd);
+            }
         }
     }
 
     close(server_fd);
     return 0;
 }
+
